80b0ea4b3b7330a1d34becf66a7c842ef69094ab
Sergio Ramagnoli <sergio.rodriguezrm@uanl.edu.mx>
2024-03-26 11:51:56 -0600

    Se modifica estilo de `NavigationComp`

--- components/NavigationComp.vue
+++ components/NavigationComp.vue
@@ -1,27 +1,26 @@
 <script setup lang="ts">
-import { useRoute } from "nuxt/app";
-import { onMounted, onUnmounted, ref, watch, nextTick } from "#imports";
-import { Transition } from "@vue/runtime-dom";
-import FareHarborLogo from "../assets/icons/FareHaborLogo";
-import CloseTransformableIcon from "./CloseTransformableIcon.vue";
-import useAppStateStore from "~/store/appStateStore";
+import { useRoute } from 'nuxt/app';
+import { useColor } from '~/composables/useColor';
+import FareHarborLogo from 'assets/icons/FareHarborLogo.vue';
+import { useIsScrollY0 } from '~/composables/useIsScrollY0';
+import { useScreenSizes } from '~/composables/useScreenSizes';
+import { useIsInTour } from '~/composables/useIsInTour';
 
-const route = useRoute();
 const routes = [
-  { path: "/", name: "Home" },
-  { path: "/tours", name: "Tours" },
+  { path: '/', name: 'Home' },
+  { path: '/tours', name: 'Tours' },
 ];
 
-const prefersDarkMode = computed<boolean>(() => appState.prefersDarkMode);
-const appState = useAppStateStore();
+const route = useRoute();
+const { colorClass } = useColor();
 const marker = ref<HTMLElement | null>(null);
 const navbar = ref<HTMLElement | null>(null);
-const selectedLink = ref<HTMLAnchorElement | null>();
-const selectedLinkWidth = ref<number | null>();
-const selectedLinkXPosition = ref<number | null>();
-const isScrollYZero = computed(() => appState.scrollYEqualToZero);
-const isNotMobile = computed(() => !appState.isMobile);
-const isDropdownOpen = computed(() => appState.isDropdownOpen);
+const selectedLink = ref<HTMLElement | null>(null);
+const selectedLinkWidth = ref<number | null>(null);
+const selectedLinkXPosition = ref<number | null>(null);
+const { isScrollY0 } = useIsScrollY0();
+const { isLargeMobile } = useScreenSizes();
+const { isInTour } = useIsInTour();
 const isModalOpen = ref(false);
 
 const widthWatcher = new ResizeObserver(() => {
@@ -32,45 +31,30 @@ function changeSelectedMarkerPosition() {
   if (selectedLink.value && marker.value) {
     selectedLinkWidth.value = selectedLink.value.clientWidth;
     selectedLinkXPosition.value = selectedLink.value.offsetLeft;
-
     marker.value.style.width = selectedLinkWidth.value
-      ? selectedLinkWidth.value + "px"
-      : "0px";
-
+      ? selectedLinkWidth.value + 'px'
+      : '0px';
     marker.value.style.left = selectedLinkXPosition.value
-      ? selectedLinkXPosition.value + "px"
-      : "0px";
+      ? selectedLinkXPosition.value + 'px'
+      : '0px';
   }
 }
 
-onMounted(async () => {
-  selectedLink.value = document.getElementById(
-    route.path,
-  ) as HTMLAnchorElement | null;
-
-  if (selectedLink.value === null && route.path.includes("tours")) {
-    selectedLink.value = document.getElementById(
-      "/tours",
-    ) as HTMLAnchorElement | null;
-
-    if (marker.value) {
-      marker.value.style.backgroundColor = "#e7e5e4";
-    }
+function setupMarker() {
+  selectedLink.value = document.getElementById(route.path);
+  if (selectedLink.value === null && route.path.includes('tours')) {
+    selectedLink.value = document.getElementById('/tours');
   }
-
   changeSelectedMarkerPosition();
-
-  if (navbar.value) {
-    widthWatcher.observe(navbar.value);
-  }
-
+  if (navbar.value) widthWatcher.observe(navbar.value);
   setTimeout(() => {
     changeSelectedMarkerPosition();
   }, 150);
+}
 
-  !FH.autoLightframe({
-    callback: () => {},
-  });
+onMounted(async () => {
+  setupMarker();
+  !FH.autoLightframe({});
 });
 
 onUnmounted(() => {
@@ -81,89 +65,76 @@ watch(
   () => route.fullPath,
   async () => {
     await nextTick();
-    selectedLink.value = document.getElementById(
-      route.path,
-    ) as HTMLAnchorElement | null;
-
-    if (selectedLink.value === null && route.path.includes("tours")) {
-      selectedLink.value = document.getElementById(
-        "/tours",
-      ) as HTMLAnchorElement | null;
-    } else if (selectedLink.value === null) {
-      if (marker.value) {
-        marker.value.style.width = "0px";
-      }
+    selectedLink.value = document.getElementById(route.path);
+    if (selectedLink.value === null && route.path.includes('tours')) {
+      selectedLink.value = document.getElementById('/tours');
+    } else if (selectedLink.value === null && marker.value) {
+      marker.value.style.width = '0px';
     }
     changeSelectedMarkerPosition();
   },
 );
 
 function toFareHarbor() {
-  return !FH.open({ shortname: "boutiquetoursmexico", flow: 278797 });
+  return !FH.open({ shortname: 'boutiquetoursmexico', flow: 278797 });
 }
-
-const isInTour = computed(() => appState.isInToursPage);
 </script>
 
 <template>
   <div
-    class="fixed w-full z-50 text-stone-950 dark:text-stone-100 backdrop-blur-3xl transition-colors duration-200"
-    :class="{
-      'bg-stone-100/90 dark:bg-stone-950/90':
-        !isScrollYZero && !isDropdownOpen && !isInTour,
-      'bg-stone-950/10': !isScrollYZero && !isDropdownOpen && isInTour,
-    }"
-    id="navbar"
-    ref="navbar"
-  >
+    class="fixed z-50 -ml-[50px] -mt-[60px] h-[200px] w-[calc(100%+100px)] bg-opacity-99 blur-xl transition-all duration-200 ease-in-out"
+    :class="!isScrollY0 && colorClass"
+  />
+  <div class="fixed z-50 w-full" ref="navbar">
     <div
-      class="flex justify-between laptop:grid laptop:grid-cols-3 items-center max-w-5xl m-auto py-4 px-6 relative"
+      class="relative m-auto flex h-20 max-w-5xl items-center justify-between px-6 py-4 laptop:grid laptop:grid-cols-3"
     >
       <div class="flex gap-8">
         <NuxtLink to="/">
           <img
             src="../assets/logos/logo.png"
             alt="Boutique Tours Mexico Logo"
-            class="h-8 laptop:h-12 brightness-[.20] dark:brightness-100 transition-all"
+            class="h-10 brightness-[.20] transition-all tablet:h-12 dark:brightness-100"
             :class="{
-              'dark:brightness-[20] dark:opacity-[.80]':
+              'dark:opacity-[.80] dark:brightness-[20]':
                 isInTour && !isModalOpen,
             }"
           />
         </NuxtLink>
       </div>
 
-      <nav class="gap-8 flex justify-center" id="menu">
+      <nav class="flex justify-center gap-4 laptop:gap-8" id="menu">
         <div
-          class="h-1 absolute bottom-4 tablet:bottom-6 transition-all rounded-md bg-opacity-50 dark:bg-opacity-50"
+          class="absolute bottom-5 h-1 rounded-[35px] bg-opacity-85 transition-all tablet:bottom-6 dark:bg-opacity-85"
           :class="{
             'bg-sky-300 dark:bg-sky-600': !isInTour,
             'bg-stone-950 dark:bg-stone-100': isInTour,
           }"
-          id="marker"
           ref="marker"
         />
-        <ul class="flex gap-8">
+        <ul class="flex gap-6">
           <li
             v-for="route in routes"
             :key="route.path"
             :id="route.path"
-            class="tracking-wider pt-1 pb-1 text-stone-950 dark:text-stone-100 uppercase"
+            class="pb-1 pt-1 uppercase tracking-wider text-stone-950 dark:text-stone-100"
           >
-            <NuxtLink :to="route.path" class="relative font-semibold">
+            <NuxtLink :to="route.path" class="relative font-medium">
               {{ route.name }}
             </NuxtLink>
           </li>
         </ul>
       </nav>
 
-      <div class="flex justify-end gap-4 items-center" v-if="isNotMobile">
+      <div class="flex items-center justify-end gap-4" v-if="!isLargeMobile">
         <button
           @click="toFareHarbor"
-          class="rounded-full font-medium items-center justify-center tablet:pr-2 uppercase flex text-lg p-2 pl-4 bg-sky-300 dark:bg-sky-600 fine:bg-opacity-50 fine:dark:bg-opacity-50 fine:hover:bg-opacity-100 transition-all duration-300 ease-in-out whitespace-nowrap"
+          class="group flex items-center justify-center whitespace-nowrap rounded-full bg-sky-200 p-2 pl-4 text-base font-medium uppercase drop-shadow-[0px_10px_25px_rgba(0,0,0,0.20)] transition-all duration-300 ease-in-out tablet:pr-2 fine:bg-opacity-70 dark:bg-sky-600"
         >
           Book now
-          <FareHarborLogo class="h-8 w-8 ml-3 fill-stone-200" />
+          <FareHarborLogo
+            class="ml-3 h-8 w-8 fill-stone-200 transition-all group-hover:rotate-90"
+          />
         </button>
       </div>
     </div>
