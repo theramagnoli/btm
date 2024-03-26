f6c0d039f0f54a5e044c03f990d4075c006b2334
Sergio Ramagnoli <sergio.rodriguezrm@uanl.edu.mx>
2024-03-26 11:57:36 -0600

    Se refactoriza página para un tour

--- pages/tours/[id].vue
+++ pages/tours/[id].vue
@@ -1,36 +1,33 @@
 <script setup lang="ts">
-import { useRoute, useRouter } from "nuxt/app";
-import { getTourById } from "assets/json/tours";
-import { CalendarIcon, ArrowLeftIcon } from "@heroicons/vue/16/solid";
-import ImageSlider from "~/components/tours/ImageSlider.vue";
-import { useMediaQuery } from "@vueuse/core";
-import { Tour, emptyTour } from "assets/entities/Tour";
+import { getTourById } from 'assets/json/tours';
+import { CalendarIcon, ArrowLeftIcon } from '@heroicons/vue/16/solid';
+import { useMediaQuery } from '@vueuse/core';
+import { Tour, emptyTour } from '~/types/Tour';
+import ImageSlider from '~/components/tours/ImageSlider.vue';
+import { tourColorDictionary } from 'assets/json/tourColorDictionary';
 
 const route = useRoute();
 const router = useRouter();
 let id = route.params.id as string;
 const tour = ref<Tour>(emptyTour);
-const isTablet = useMediaQuery("(max-width: 1000px)");
+const isWidthBellow1000px = useMediaQuery('(max-width: 1000px)');
 
 const untypedTour = getTourById(id);
-if (untypedTour) {
-  tour.value = untypedTour;
-} else {
-  router.push({ path: "/404" });
-}
+if (untypedTour) tour.value = untypedTour;
+else router.push({ path: '/404' });
 
 useHeadSafe({
   title: `${tour.value?.title} Tour | Boutique Tours Mexico`,
-  meta: [{ name: "description", content: tour.value?.desc[0] }],
+  meta: [{ name: 'description', content: tour.value?.desc[0] }],
 });
 
 onErrorCaptured(() => {
-  router.push({ path: "/404" });
+  router.push({ path: '/404' });
 });
 
 function toFareHarborItem(itemId: string) {
   return !FH.open({
-    shortname: "boutiquetoursmexico",
+    shortname: 'boutiquetoursmexico',
     flow: 278797,
     view: { item: Number(itemId) },
   });
@@ -38,55 +35,43 @@ function toFareHarborItem(itemId: string) {
 </script>
 
 <template>
-  <div class="container-div">
+  <div
+    class="m-auto flex min-h-[calc(100vh-234px)] flex-col overflow-hidden pb-16 pt-[120px] text-stone-900 laptop:max-w-5xl laptop:pt-[100px] dark:text-stone-200"
+  >
     <div
-      class="justify-start laptop:gap-8 laptop:mt-8 px-0 laptop:px-6"
+      class="justify-start px-0 laptop:mt-8 laptop:gap-8 laptop:px-6"
       :class="{
-        'grid grid-cols-[6fr_4fr]': !isTablet,
-        'flex flex-col': isTablet,
+        'grid grid-cols-[6fr_4fr]': !isWidthBellow1000px,
+        'flex flex-col': isWidthBellow1000px,
       }"
     >
-      <div class="flex flex-col gap-4 pt-4 pb-8 px-6 laptop:px-0 w-full">
-        <div class="flex gap-8">
+      <div class="flex w-full flex-col gap-4 px-6 pb-8 laptop:px-0">
+        <div class="flex gap-4">
           <NuxtLink
             to="/tours"
-            class="flex items-center gap-2 opacity-60 hover:opacity-100 transition-all duration-30"
+            :class="`flex aspect-square size-10 items-center justify-center rounded-full bg-opacity-50 p-2 transition-all duration-300 hover:bg-opacity-80 dark:bg-opacity-50 bg-${tourColorDictionary[tour.id]}-50 dark:bg-${tourColorDictionary[tour.id]}-900`"
           >
-            <ArrowLeftIcon class="size-5" /> Return
+            <ArrowLeftIcon class="size-5" />
           </NuxtLink>
           <button
             @click="toFareHarborItem(tour.book)"
-            class="flex items-center gap-2 opacity-60 hover:opacity-100 transition-all duration-30"
+            :class="`flex h-10 items-center gap-2 rounded-full bg-opacity-50 p-2 pl-3 pr-4 transition-all duration-300 hover:bg-opacity-80 dark:bg-opacity-50 bg-${tourColorDictionary[tour.id]}-50 dark:bg-${tourColorDictionary[tour.id]}-900`"
           >
             <CalendarIcon class="size-5" /> Book this tour
           </button>
         </div>
         <h1>{{ tour.title }} Tour</h1>
-        <div class="paragraph-div px-0 mt-4 text-left" v-if="!isTablet">
-          <p
-            v-for="paragraph of tour.desc"
-            :key="paragraph"
-            class="hyphens-auto"
-          >
-            {{ paragraph }}
-          </p>
-        </div>
+        <Paragraphs :paragraphs="tour.desc" v-if="!isWidthBellow1000px" />
       </div>
       <ImageSlider
-        :images="tour.imgs"
-        :orientation="isTablet ? 'horizontal' : 'vertical'"
+        :images="tour.images"
+        :orientation="isWidthBellow1000px ? 'horizontal' : 'vertical'"
       />
-      <div class="paragraph-div mt-6 text-left text-stone-200" v-if="isTablet">
-        <p v-for="paragraph of tour.desc" :key="paragraph" class="hyphens-auto">
-          {{ paragraph }}
-        </p>
-      </div>
+      <Paragraphs
+        :paragraphs="tour.desc"
+        v-if="isWidthBellow1000px"
+        class="mt-6 px-6"
+      />
     </div>
   </div>
 </template>
-
-<style lang="postcss">
-.background {
-  background-color: green !important;
-}
-</style>
