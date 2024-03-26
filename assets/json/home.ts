3a3f733981144eea6a2324d164bbbf657baafb26
Sergio Ramagnoli <sergio.rodriguezrm@uanl.edu.mx>
2024-03-26 11:50:18 -0600

    Se modifica contenido de página

--- assets/json/home.ts
+++ assets/json/home.ts
@@ -1,51 +1,29 @@
-import type { Review } from "~/types/Review";
-export const description = [
-  "Enjoy a day full of fun, adventure, history, delicious food and unforgettable memories. We will take you to the most beautiful Cenotes, impressive Mayan archaeological sites or idyllic places.",
-  "We suggest some ideas for your activities but you can also customize your own experience.",
-  "Join us, we love our country and we have a passion for it and we would love to share all of our knowledge and experience with you.",
+import type { Review } from '~/types/Review';
+export const pitch = [
+  'Enjoy a day full of fun, adventure, history, delicious food and unforgettable memories. We will take you to the most beautiful Cenotes, impressive Mayan archaeological sites or idyllic places.',
+  'We suggest some ideas for your activities but you can customize your own experience. Join us, we love our country and we have a passion for it and we would love to share all of our knowledge and experience with you.',
 ];
-
-/*
- Benefits
-  * Immersion in the local culture
-  * An inspiring and rewarding travel experience
-  * Private tour guide,
-  * All entrance fees,
-  * Snacks and drinks,
-  * Delicious lunch,
-  * Travel comfortably in an air-conditioned van,
-  * Snorkeling equipment,
-  * Everything included in one price without surprises,
-*/
 
 export const benefits = [
-  {
-    description:
-      "Immerse in the local culture, make memories that matter with a rewarding travel experience",
-    image:
-      "https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/tours%2FChichenItza%2FEatingAsMayans.JPG?alt=media&token=05974e12-8bc4-48d0-9472-0d58eddec843",
-  },
-  {
-    description:
-      "Let your private guide lead the way, enjoy worry-free travel with all entrance fees included in one price",
-    image:
-      "https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2Fbenefits%2Ffather%20an%20son%20in%20chichen.jpg?alt=media&token=0956ac5c-a4f6-4a4f-abce-1a5b8d38163b",
-  },
-  {
-    description:
-      "Travel comfortably in an air-conditioned van, equipped for your comfort",
-    image:
-      "https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2Fbenefits%2Fwomen%20in%20cenote.jpg?alt=media&token=f5dc7d87-64f7-4172-b092-8fe0d1b5bf92",
-  },
+  'Immersion in the local culture',
+  'An inspiring and rewarding travel experience',
+  'Private tour guide',
+  'All entrance fees',
+  'Drinks',
+  'Delicious Mexican lunch',
+  'Snorkeling equipment',
+  'Comfortable travel in an air-conditioned van',
+  'Everything included in one price without surprises',
 ];
+
 export const faq = [
   {
-    question: "How long does a tour last?",
+    question: 'How long does a tour last?',
     answer:
-      "An average tour lasts about 6 to 8 hours, but remember: they are highly customizable, so we can adapt to your needs and/or preferences.",
+      'An average tour lasts about 6 to 8 hours, but remember: they are highly customizable, so we can adapt to your needs and/or preferences.',
   },
   {
-    question: "What is our refund policy?",
+    question: 'What is our refund policy?',
     answer:
       "If you cancel with 48 hours of anticipation, you get 80% refund due to taxes and commissions paid in Mexico. If you cancel with 24 hours of anticipation, you get 50% of refund. If you cancel with less than 24 hour of anticipation, you don't get a refund.",
   },
@@ -53,57 +31,55 @@ export const faq = [
 
 export const testimonials: Review[] = [
   {
-    author: "Mike&Chris",
-    headline: "Was like having a family member show us around Mexico.",
-    url: "https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r880383776-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html",
+    author: 'Mike&Chris',
+    headline: 'Was like having a family member show us around Mexico.',
+    url: 'https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r880383776-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html',
   },
   {
-    author: "T F",
+    author: 'T F',
     headline:
       "It doesn't get more authentic then this and we couldn't have asked for better people to spend the day with!",
-    url: "https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r844359028-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html",
+    url: 'https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r844359028-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html',
   },
   {
-    author: "kennynyc",
+    author: 'kennynyc',
     headline:
-      "The quality of the tour, the beauty of the sites, and the expertise of our guide all came together to make for an unforgettable day.",
-    url: "https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r878694155-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html",
+      'The quality of the tour, the beauty of the sites, and the expertise of our guide all came together to make for an unforgettable day.',
+    url: 'https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r878694155-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html',
   },
-
   {
-    author: "Charlene W",
+    author: 'Alisha',
+    headline:
+      'They are such great people and the overall experience was just amazing. I honestly cant even think of any cons. If you want to have a great tour with knowledgeable, fun people, I would definitely choose them!',
+    url: 'https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r850050361-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html',
+  },
+  {
+    author: 'Dana S',
+    headline: 'Absolutely amazing! Authentic experiences off the beaten path!',
+    url: 'https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r877139933-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html',
+  },
+  {
+    author: 'Amy P',
+    headline:
+      'Boutique Tours Mexico is a wonderful family-owned business that treat you like family. There tours are uniquely planned and executed. It is pure perfection from start to finish.',
+    url: 'https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r875474509-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html',
+  },
+  {
+    author: 'Ryan',
+    headline:
+      'Let me put this way, I loved this tour so much I booked three more excursions with this company. ',
+    url: 'https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r854547400-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html',
+  },
+  {
+    author: 'carolinacarvelli',
+    headline:
+      'Incredible experience! So much more than I could have imagined - a must do!',
+    url: 'https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r802413862-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html',
+  },
+  {
+    author: 'Charlene W',
     headline:
       "I've been on many tours, both public and private, and this was one of the best given the level of service, quality of activities, and tour guide personalities.",
-    url: "https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r866829939-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html",
-  },
-  {
-    author: "Reuben F",
-    headline: "One of the best experiences of our life!",
-    url: "https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r877139933-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html",
-  },
-  {
-    author: "Amy P",
-    headline:
-      "Boutique Tours Mexico is a wonderful family-owned business that treat you like family. There tours are uniquely planned and executed. It is pure perfection from start to finish.",
-    url: "https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r875474509-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html",
-  },
-
-  {
-    author: "carolinacarvelli",
-    headline:
-      "Incredible experience! So much more than I could have imagined - a must do!",
-    url: "https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r802413862-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html",
-  },
-  {
-    author: "Alisha",
-    headline:
-      "They are such great people and the overall experience was just amazing. I honestly cant even think of any cons. If you want to have a great tour with knowledgeable, fun people, I would definitely choose them!",
-    url: "https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r850050361-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html",
-  },
-  {
-    author: "Ryan",
-    headline:
-      "Let me put this way, I loved this tour so much I booked three more excursions with this company. ",
-    url: "https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r854547400-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html",
+    url: 'https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r866829939-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html',
   },
 ];
--- assets/json/reviews.ts
+++ assets/json/reviews.ts
@@ -1,82 +1,82 @@
-import type { Review } from "~/types/Review";
+import type { Review } from '~/types/Review';
 const reviews: Record<string, Review> = {
-  "1": {
-    headline: "Unique Mexican Adventure",
-    author: "PGrizzy17 on TripAdvisor",
-    img: "https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2FGroupCasaCenote.jpg?alt=media",
-    url: "https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r837354001-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html",
+  '1': {
+    headline: 'Unique Mexican Adventure',
+    author: 'PGrizzy17 on TripAdvisor',
+    img: 'https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2FGroupCasaCenote.jpg?alt=media',
+    url: 'https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r837354001-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html',
   },
-  "2": {
-    headline: "Unforgettable",
-    author: "Dan O on TripAdvisor",
-    img: "https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2FCoupleChichenitza.jpg?alt=media",
-    url: "https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r832629666-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html",
+  '2': {
+    headline: 'Unforgettable',
+    author: 'Dan O on TripAdvisor',
+    img: 'https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2FCoupleChichenitza.jpg?alt=media',
+    url: 'https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r832629666-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html',
   },
-  "3": {
-    headline: "And that my friends is what you call EXPERIENCE!",
-    author: "Mrstravgal on TripAdvisor",
-    img: "https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2FExploringCenote.jpg?alt=media",
-    url: "https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r469173877-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html",
+  '3': {
+    headline: 'And that my friends is what you call EXPERIENCE!',
+    author: 'Mrstravgal on TripAdvisor',
+    img: 'https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2FExploringCenote.jpg?alt=media',
+    url: 'https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r469173877-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html',
   },
-  "4": {
-    headline: "Literally Perfect",
-    author: "Drewsomeness on TripAdvisor",
-    img: "https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2FPuntaAllen.png?alt=media",
-    url: "https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r783300881-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html",
+  '4': {
+    headline: 'Literally Perfect',
+    author: 'Drewsomeness on TripAdvisor',
+    img: 'https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2FPuntaAllen.png?alt=media',
+    url: 'https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r783300881-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html',
   },
-  "5": {
-    headline: "Memory to last a lifetime!",
-    author: "717christinei on TripAdvisor",
-    img: "https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2FMayanAdventure.jpg?alt=media",
-    url: "https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r831137004-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html",
+  '5': {
+    headline: 'Memory to last a lifetime!',
+    author: '717christinei on TripAdvisor',
+    img: 'https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2FMayanAdventure.jpg?alt=media',
+    url: 'https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r831137004-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html',
   },
-  "6": {
-    headline: "PERFECTION!",
-    author: "4FredandGypsy on TripAdvisor",
-    img: "https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2FCenote.png?alt=media",
-    url: "https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r622144075-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html",
+  '6': {
+    headline: 'PERFECTION!',
+    author: '4FredandGypsy on TripAdvisor',
+    img: 'https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2FCenote.png?alt=media',
+    url: 'https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r622144075-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html',
   },
-  "7": {
-    headline: "BEYOND WORDS",
-    author: "Tulumefriends on TripAdvisor",
-    img: "https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2FSianKaan.jpg?alt=media",
-    url: "https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r774664694-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html",
+  '7': {
+    headline: 'BEYOND WORDS',
+    author: 'Tulumefriends on TripAdvisor',
+    img: 'https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2FSianKaan.jpg?alt=media',
+    url: 'https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r774664694-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html',
   },
-  "8": {
-    headline: "Exciting, breathtaking...",
-    author: "Navigate277912",
-    img: "https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2FClosedCenote.jpg?alt=media",
-    url: "https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r539528968-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html",
+  '8': {
+    headline: 'Exciting, breathtaking...',
+    author: 'Navigate277912',
+    img: 'https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2FClosedCenote.jpg?alt=media',
+    url: 'https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r539528968-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html',
   },
-  "9": {
-    headline: "AMAZING! A true Mayan experience",
-    author: "angelicaramos847 on TripAdvisor",
-    img: "https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2FCovidTimesAtCoba.jpg?alt=media",
-    url: "https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r751247919-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html",
+  '9': {
+    headline: 'AMAZING! A true Mayan experience',
+    author: 'angelicaramos847 on TripAdvisor',
+    img: 'https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2FCovidTimesAtCoba.jpg?alt=media',
+    url: 'https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r751247919-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html',
   },
-  "10": {
-    headline: "BTM is simply world class",
-    author: "F5955DRjuanv on TripAdvisor",
-    img: "https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2FCoupleSnorkeling.png?alt=media",
-    url: "https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r781584720-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html",
+  '10': {
+    headline: 'BTM is simply world class',
+    author: 'F5955DRjuanv on TripAdvisor',
+    img: 'https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2FCoupleSnorkeling.png?alt=media',
+    url: 'https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r781584720-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html',
   },
-  "11": {
-    headline: "One of the highlights of my life",
-    author: "TamkinAminCollins on TripAdvisor",
-    img: "https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2FLazyRiver.jpg?alt=media",
-    url: "https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r913515147-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html",
+  '11': {
+    headline: 'One of the highlights of my life',
+    author: 'TamkinAminCollins on TripAdvisor',
+    img: 'https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2FLazyRiver.jpg?alt=media',
+    url: 'https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r913515147-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html',
   },
-  "12": {
-    headline: "A MUST DO EXPERIENCE!",
-    author: "763kimmh on TripAdvisor",
-    img: "https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2FInCoba.jpg?alt=media",
-    url: "https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r802519751-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html",
+  '12': {
+    headline: 'A MUST DO EXPERIENCE!',
+    author: '763kimmh on TripAdvisor',
+    img: 'https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2FInCoba.jpg?alt=media',
+    url: 'https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r802519751-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html',
   },
-  "13": {
-    headline: "SHEEEEEESH",
-    author: "jcj2180 on TripAdvisor",
-    img: "https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2FGroupChichenitza.jpg?alt=media",
-    url: "https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r821307218-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html",
+  '13': {
+    headline: 'SHEEEEEESH',
+    author: 'jcj2180 on TripAdvisor',
+    img: 'https://firebasestorage.googleapis.com/v0/b/boutique-tours-mexico.appspot.com/o/inicio%2FGroupChichenitza.jpg?alt=media',
+    url: 'https://www.tripadvisor.com/ShowUserReviews-g150812-d12246056-r821307218-Boutique_Tours_Mexico-Playa_del_Carmen_Yucatan_Peninsula.html',
   },
 };
 
