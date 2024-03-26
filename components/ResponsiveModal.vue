25d2acf359db9459bb300b81526f05792555df88
Sergio Ramagnoli <sergio.rodriguezrm@uanl.edu.mx>
2024-03-26 11:58:35 -0600

    Se formatea proyecto con prettier

--- components/ResponsiveModal.vue
+++ components/ResponsiveModal.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import { XMarkIcon } from "@heroicons/vue/16/solid";
-import { onClickOutside } from "@vueuse/core";
+import { XMarkIcon } from '@heroicons/vue/16/solid';
+import { onClickOutside } from '@vueuse/core';
 
-const emit = defineEmits(["close"]);
+const emit = defineEmits(['close']);
 const modal = ref<HTMLElement | null>(null);
 
 defineProps<{
@@ -12,7 +12,7 @@ defineProps<{
 }>();
 
 onClickOutside(modal, () => {
-  emit("close");
+  emit('close');
 });
 </script>
 
@@ -21,22 +21,22 @@ onClickOutside(modal, () => {
     <Transition name="fade" duration="50">
       <div
         v-if="isOpen"
-        class="fixed top-0 left-0 w-screen h-[100dvh] z-40 flex justify-center items-end laptop:items-center filter bg-stone-100/70 dark:bg-stone-950/70"
+        class="fixed left-0 top-0 z-40 flex h-[100dvh] w-screen items-end justify-center bg-stone-100/70 filter laptop:items-center dark:bg-stone-950/70"
         :class="{
           'backdrop-blur-[60px]': blurBackground,
         }"
       >
         <Transition name="fade" duration="100">
           <div
-            class="relative bg-stone-100 dark:bg-stone-950 rounded-t-md laptop:rounded-md w-full max-w-lg p-5 flex flex-col gap-4 text-stone-950 dark:text-stone-100 drop-shadow-[0px_10px_25px_rgba(0,0,0,0.2)] dark:drop-shadow-[0px_10px_25px_rgba(255,255,255,0.2)] pb-10 laptop:pb-6"
+            class="mb:pt-5 relative flex w-full max-w-lg flex-col gap-4 rounded-t-[30px] bg-stone-100 p-5 pb-10 pt-8 text-stone-950 drop-shadow-[0px_10px_25px_rgba(0,0,0,0.2)] laptop:rounded-[30px] laptop:pb-6 dark:bg-stone-950 dark:text-stone-100 dark:drop-shadow-[0px_10px_25px_rgba(255,255,255,0.2)]"
             ref="modal"
           >
             <button
-              class="hidden absolute -top-10 right-0 p-1 bg-stone-100 dark:bg-stone-950 rounded-md laptop:flex justify-center items-center size-8 transition-all duration-300 ease-in-out"
+              class="absolute -top-10 right-0 hidden size-8 items-center justify-center rounded-full bg-stone-100 laptop:flex dark:bg-stone-950"
               @click="emit('close')"
             >
               <XMarkIcon
-                class="size-5 fill-stone-950 dark:fill-stone-100 duration-300 ease-in-out cursor-pointer"
+                class="size-5 cursor-pointer fill-stone-950 duration-300 ease-in-out dark:fill-stone-100"
               />
             </button>
             <slot />
--- pages/tours/index.vue
+++ pages/tours/index.vue
@@ -1,30 +1,32 @@
 <script setup lang="ts">
-import ToursShowcase from "~/components/tours/ToursShowcase.vue";
+import ToursShowcase from '~/components/tours/ToursShowcase.vue';
 
 useHeadSafe({
-  title: "Tours | Boutique Tours Mexico",
+  title: 'Tours | Boutique Tours Mexico',
   meta: [
     {
-      name: "description",
+      name: 'description',
       content:
-        "Discover our tour catalogue. From the most popular destinations to the most remote and unknown places in Mexico. We are sure you will find the perfect tour for you.",
+        'Discover our tour catalogue. From the most popular destinations to the most remote and unknown places in Mexico. We are sure you will find the perfect tour for you.',
     },
   ],
 });
 </script>
 
 <template>
-  <div class="container-div">
-    <div class="grid px-6 gap-4 mt-4">
+  <div
+    class="m-auto flex min-h-[calc(100vh-234px)] flex-col overflow-hidden pb-16 pt-[120px] text-stone-900 laptop:max-w-5xl laptop:pt-[100px] dark:text-stone-200"
+  >
+    <div class="grid gap-4 px-6 laptop:pt-6 desktop:mt-4">
       <h1
-        class="text-3xl tablet:text-4xl tracking-tight bg-gradient-to-t from-yellow-600 to-emerald-600 bg-clip-text text-transparent dark:text-transparent"
+        class="bg-gradient-to-t from-yellow-600 to-emerald-600 bg-clip-text text-4xl tracking-tight text-transparent tablet:text-5xl laptop:mb-4 dark:text-transparent"
       >
         Find your <br />
         perfect <br />
         destination
       </h1>
 
-      <div class="grid gap-4 mt-4">
+      <div class="mt-2 grid gap-4">
         <ToursShowcase />
       </div>
     </div>
