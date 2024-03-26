f60159236c46f63bdce3725d9881cfefc37ce2a6
Sergio Ramagnoli <sergio.rodriguezrm@uanl.edu.mx>
2024-03-26 11:53:03 -0600

    Se modifican estilos de `ImageSlider` y `ReviewsSlider`

--- components/ReviewsSlider.vue
+++ components/ReviewsSlider.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-import { onUnmounted, ref } from "#imports";
-import reviews from "assets/json/reviews";
+import { onUnmounted, ref } from '#imports';
+import reviews from 'assets/json/reviews';
 import {
   ChevronLeftIcon,
   ChevronRightIcon,
   StarIcon,
-} from "@heroicons/vue/16/solid";
-import { Review } from "~/types/Review";
+} from '@heroicons/vue/16/solid';
+import { Review } from '~/types/Review';
 
 const emit = defineEmits<{
-  (event: "review-clicked", data: Review): void;
+  (event: 'review-clicked', data: Review): void;
 }>();
 
 const sliderIsPlaying = ref(true);
@@ -22,7 +22,7 @@ const typedReviews = ref<Review[]>(Object.values(reviews));
 function preloadImages() {
   for (let i = 0; i < typedReviews.value.length; i++) {
     const img = new Image();
-    img.src = typedReviews.value[i].img ?? "";
+    img.src = typedReviews.value[i].img ?? '';
     isImageLoaded.value.push(false);
     img.onload = () => {
       isImageLoaded.value[i] = true;
@@ -33,7 +33,7 @@ function preloadImages() {
 preloadImages();
 
 function scrollNext() {
-  const slider = document.getElementById("slider");
+  const slider = document.getElementById('slider:reviews');
   if (slider) {
     let width = slider.scrollWidth - slideDesktopPx;
     const scroll = (slider.scrollLeft += slideDesktopPx);
@@ -48,7 +48,7 @@ function scrollNext() {
 }
 
 function scrollBack() {
-  const slider = document.getElementById("slider");
+  const slider = document.getElementById('slider:reviews');
   if (slider) {
     const scroll = (slider.scrollLeft -= slideDesktopPx);
     if (scroll < -400) {
@@ -65,7 +65,7 @@ function restartInterval() {
 
 function autoplaySlider() {
   sliderInterval.value = setInterval(() => {
-    if (window.matchMedia("(min-width: 1000px)").matches && sliderIsPlaying) {
+    if (window.matchMedia('(min-width: 1000px)').matches && sliderIsPlaying) {
       scrollNext();
     }
   }, msBetweenSlides);
@@ -83,26 +83,28 @@ autoplaySlider();
 <template>
   <div class="relative">
     <ul
-      class="flex snap-x snap-mandatory overflow-x-auto laptop:rounded-md max-w-[100dvw] laptop:max-w-[550px] gap-6 laptop:gap-8 scroll-smooth"
-      id="slider"
+      class="desktop::mt-0 mt-6 flex max-w-[100dvw] snap-x snap-mandatory gap-6 overflow-x-auto scroll-smooth laptop:max-w-[545px] laptop:gap-8 laptop:rounded-[30px]"
+      id="slider:reviews"
     >
       <li
         v-for="(review, index) in typedReviews"
         :key="index"
-        class="snap-center laptop:snap-start laptop:last:snap-end laptop:first:snap-start first:ml-6 last:mr-6 cursor-pointer group"
+        class="group cursor-pointer snap-center first:ml-6 last:mr-6 laptop:snap-start laptop:first:snap-start laptop:last:snap-end"
         @click="emit('review-clicked', review)"
       >
-        <div class="relative overflow-hidden m-auto transition-all rounded-md">
+        <div
+          class="relative m-auto overflow-hidden rounded-[30px] transition-all"
+        >
           <Transition name="fade" :duration="400">
             <NuxtImg
               format="webp"
               :src="review.img ?? ''"
               :lazy="true"
-              class="absolute inset-0 w-full h-full object-cover object-center fine:group-hover:brightness-[0.8] transition-all duration-300 ease-in-out"
+              class="absolute inset-0 h-full w-full object-cover object-center transition-all duration-300 ease-in-out"
               v-if="isImageLoaded[index]"
             />
             <div
-              class="absolute inset-0 w-full h-full bg-stone-300 dark:stone-600 opacity-30 animate-pulse rounded-md laptop:rounded-none"
+              class="dark:stone-600 absolute inset-0 h-full w-full animate-pulse rounded-[30px] bg-stone-300 opacity-30 laptop:rounded-none"
               v-else
             />
           </Transition>
@@ -111,18 +113,18 @@ autoplaySlider();
             v-if="isImageLoaded[index]"
           />
           <div
-            class="relative h-[27.5rem] laptop:h-[27.5rem] w-[22rem] max-w-[75dvw] laptop:w-[22rem] pb-8 flex flex-col-reverse justify-between items-center m-auto px-6"
+            class="relative m-auto flex h-[27.5rem] w-[22rem] max-w-[75dvw] flex-col-reverse items-center justify-between px-6 pb-8 laptop:h-[27.5rem] laptop:w-[22rem]"
           >
-            <div class="grid justify-center w-full">
+            <div class="grid w-full justify-center">
               <div class="flex justify-center gap-1">
                 <StarIcon
                   v-for="n in 5"
                   :key="n"
-                  class="size-5 fill-amber-400 dark:fill-amber-600 drop-shadow-md"
+                  class="size-5 fill-amber-400 drop-shadow-md dark:fill-amber-600"
                 />
               </div>
               <p
-                class="mt-2 text-stone-100 text-center laptop:text-xl drop-shadow-xl font-medium leading-snug tracking-loose"
+                class="tracking-loose mt-2 text-center font-medium leading-snug text-stone-100 drop-shadow-xl laptop:text-xl"
               >
                 {{ review.headline }}
               </p>
@@ -132,19 +134,19 @@ autoplaySlider();
       </li>
     </ul>
     <div
-      class="hidden tablet:flex absolute top-4 right-10 laptop:right-4 gap-2 p-1 rounded-full bg-stone-100/60 dark:bg-stone-800/60"
+      class="desktop::mt-0 absolute right-10 top-4 mt-6 hidden gap-2 rounded-full bg-stone-100/40 p-1 backdrop-blur-xl tablet:flex laptop:right-4 dark:bg-stone-800/40"
     >
       <button
         @click="scrollBack"
-        class="grid items-center justify-center h-6 w-6 rounded-full transition-all text-stone-900 dark:text-stone-200 dark:bg-black/40 bg-gray-100/40 hover:bg-stone-200 dark:hover:bg-black"
+        class="grid h-6 w-6 items-center justify-center rounded-full bg-gray-100/40 text-stone-900 transition-all hover:bg-stone-200 dark:bg-black/40 dark:text-stone-200 dark:hover:bg-black"
       >
-        <ChevronLeftIcon class="w-6 h-6" />
+        <ChevronLeftIcon class="h-6 w-6" />
       </button>
       <button
         @click="scrollNext"
-        class="grid items-center justify-center h-6 w-6 rounded-full transition-all text-stone-900 dark:text-stone-200 dark:bg-black/40 bg-gray-100/40 hover:bg-stone-200 dark:hover:bg-black"
+        class="grid h-6 w-6 items-center justify-center rounded-full bg-gray-100/40 text-stone-900 transition-all hover:bg-stone-200 dark:bg-black/40 dark:text-stone-200 dark:hover:bg-black"
       >
-        <ChevronRightIcon class="w-6 h-6" />
+        <ChevronRightIcon class="h-6 w-6" />
       </button>
     </div>
   </div>
--- components/tours/ImageSlider.vue
+++ components/tours/ImageSlider.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/vue/16/solid";
-import { ref } from "#imports";
-import { useMediaQuery } from "@vueuse/core";
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/vue/16/solid';
+import { ref } from 'vue';
+import { useMediaQuery } from '@vueuse/core';
 
-const isMobile = useMediaQuery("(max-width: 520px)");
+const isMobile = useMediaQuery('(max-width: 520px)');
 
 const props = defineProps<{
   images: {
     src: string;
     alt: string;
-    ar: "4:3" | "3:4";
+    ar: '4:3' | '3:4';
   }[];
-  orientation: "horizontal" | "vertical";
+  orientation: 'horizontal' | 'vertical';
 }>();
 
 const sliderIsPlaying = true;
 const sliderInterval = ref<NodeJS.Timer | null>();
-const msBetweenSlides = 3500;
+const msBetweenSlides = 4000;
 const verticalSlideLengthInPx = 350;
 const horizontalSlideLengthInPx = 200;
 const verticalSliderOffset = 100;
@@ -37,9 +37,9 @@ function preloadImages() {
 preloadImages();
 
 function scrollNext() {
-  const slider = document.getElementById("slider");
+  const slider = document.getElementById('slider');
   if (slider) {
-    if (props.orientation === "horizontal") {
+    if (props.orientation === 'horizontal') {
       let width = slider.scrollWidth - horizontalSlideLengthInPx;
       const scroll = (slider.scrollLeft += horizontalSlideLengthInPx);
       if (scroll > width - horizontalSliderOffset) {
@@ -57,9 +57,9 @@ function scrollNext() {
 }
 
 function scrollBack() {
-  const slider = document.getElementById("slider");
+  const slider = document.getElementById('slider');
   if (slider) {
-    if (props.orientation === "horizontal") {
+    if (props.orientation === 'horizontal') {
       let width = slider.scrollWidth - horizontalSlideLengthInPx;
       const scroll = (slider.scrollLeft -= horizontalSlideLengthInPx);
       if (scroll <= -horizontalSlideLengthInPx) {
@@ -83,7 +83,7 @@ function restartInterval() {
 
 function autoplaySlider() {
   sliderInterval.value = setInterval(() => {
-    if (window.matchMedia("(min-width: 1000px)").matches && sliderIsPlaying) {
+    if (window.matchMedia('(min-width: 1000px)').matches && sliderIsPlaying) {
       scrollNext();
     }
   }, msBetweenSlides);
@@ -101,12 +101,12 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div class="w-full relative">
+  <div class="relative w-full">
     <ul
-      class="flex snap-mandatory scroll-smooth laptop:rounded-md overflow-hidden"
+      class="flex snap-mandatory overflow-hidden scroll-smooth laptop:rounded-[35px]"
       :class="{
         'snap-x gap-x-6 overflow-x-auto': props.orientation === 'horizontal',
-        'snap-y gap-y-6 laptop:h-[90dvh] laptop:max-h-[725px] flex-col pointer-events-none overflow-y-auto':
+        'pointer-events-none snap-y flex-col gap-y-6 overflow-y-auto laptop:h-[90dvh] laptop:max-h-[725px]':
           props.orientation === 'vertical',
       }"
       id="slider"
@@ -114,14 +114,16 @@ onUnmounted(() => {
       <li
         v-for="(img, i) of images"
         :key="i"
-        class="snap-center laptop:snap-start first:ml-6 last:mr-6 laptop:first:ml-0 laptop:last:mr-0 max-w-[75dvw]"
+        class="max-w-[75dvw] snap-center first:ml-6 last:mr-6 laptop:snap-start laptop:first:ml-0 laptop:last:mr-0"
+        :class="{
+          'h-[320px]': props.orientation === 'horizontal',
+        }"
       >
         <div
-          class="overflow-hidden transition-all rounded-md"
+          class="relative h-full w-full overflow-hidden rounded-[35px] transition-all"
           :class="{
-            'aspect-[4/4] tablet:aspect-[4/4]': img.ar === '4:3',
+            'aspect-[4/4]': img.ar === '4:3',
             'aspect-[3/4]': img.ar === '3:4',
-            'h-[320px]': props.orientation === 'horizontal',
           }"
         >
           <!-- TODO: Fix transition -->
@@ -130,35 +132,35 @@ onUnmounted(() => {
             :alt="img.alt"
             format="webp"
             :lazy="true"
-            class="w-full h-full object-cover object-center"
+            class="absolute inset-0 h-full w-full object-cover object-center"
             v-if="isImageLoaded[i]"
           />
           <div
-            class="w-full h-full bg-gray-500 opacity-30 animate-pulse rounded-md laptop:rounded-none"
+            class="h-full w-full animate-pulse rounded-[35px] bg-gray-500 opacity-30 laptop:rounded-none"
             v-else
           />
         </div>
       </li>
     </ul>
     <div
-      class="absolute top-4 right-10 laptop:right-4 gap-2 p-1 rounded-full bg-stone-100/60 dark:bg-stone-800/60 tablet:flex flex-row laptop:flex-col"
+      class="absolute right-10 top-4 flex-row gap-2 rounded-full bg-stone-100/40 p-1 backdrop-blur-xl tablet:flex laptop:right-4 laptop:flex-col dark:bg-stone-800/40"
       v-if="!isMobile"
     >
       <button
         @click="scrollBack"
-        class="grid items-center justify-center h-6 w-6 rounded-full transition-all text-stone-900 dark:text-stone-200 dark:bg-black/40 bg-gray-100/40 hover:bg-stone-200 dark:hover:bg-black"
+        class="grid size-6 items-center justify-center rounded-full bg-gray-100/40 text-stone-900 transition-all hover:bg-stone-200 dark:bg-black/40 dark:text-stone-200 dark:hover:bg-black"
       >
         <ChevronLeftIcon
-          class="w-6 h-6 laptop:rotate-90"
+          class="size-6 laptop:rotate-90"
           :class="props.orientation === 'vertical' ? 'mb-[3px]' : 'mr-[2px]'"
         />
       </button>
       <button
         @click="scrollNext"
-        class="grid items-center justify-center h-6 w-6 rounded-full transition-all text-stone-900 dark:text-stone-200 dark:bg-black/40 bg-gray-100/40 hover:bg-stone-200 dark:hover:bg-black"
+        class="grid size-6 items-center justify-center rounded-full bg-gray-100/40 text-stone-900 transition-all hover:bg-stone-200 dark:bg-black/40 dark:text-stone-200 dark:hover:bg-black"
       >
         <ChevronRightIcon
-          class="w-6 h-6 laptop:rotate-90"
+          class="size-6 laptop:rotate-90"
           :class="props.orientation === 'vertical' ? 'mb-[3px]' : 'ml-[2px]'"
         />
       </button>
