903842274eb757fcbe381e92fd24924297bcb76b
Sergio Ramagnoli <sergio.rodriguezrm@uanl.edu.mx>
2024-03-26 11:46:40 -0600

    Se modifican estilos globales

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,82 +1,86 @@
 module.exports = {
   content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/*.vue",
-    "./pages/**/*.vue",
-    "./app.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/*.vue',
+    './pages/**/*.vue',
+    './app.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
   ],
-  darkMode: "class",
+  darkMode: 'class',
   theme: {
     fontFamily: {
-      sans: ["Syne", "sans-serif"],
-      display: ["Unbounded", "sans-serif"],
+      sans: ['Lexend', 'sans-serif'],
+      display: ['Lexend', 'sans-serif'],
     },
     screens: {
-      tablet: "520px",
-      laptop: "1000px",
-      desktop: "1400px",
-      768: "768px",
-      coarse: { raw: "(pointer: coarse)" },
-      fine: { raw: "(pointer: fine)" },
+      tablet: '550px',
+      laptop: '1000px',
+      desktop: '1400px',
+      '[700px]': '700px',
+      '[768px]': '768px',
+      coarse: { raw: '(pointer: coarse)' },
+      fine: { raw: '(pointer: fine)' },
     },
     extend: {
+      opacity: {
+        99: '0.99',
+      },
       gridTemplateColumns: {
-        "300px": "repeat(auto-fill, minmax(300px, 1fr))",
-        "350px": "repeat(auto-fill, minmax(350px, 1fr))",
+        '300px': 'repeat(auto-fill, minmax(300px, 1fr))',
+        '350px': 'repeat(auto-fill, minmax(350px, 1fr))',
       },
       backgroundImage: {
-        "gradient-radial-to-t":
-          "radial-gradient(110% 100% at 50% 115%, var(--tw-gradient-stops))",
+        'gradient-radial-to-t':
+          'radial-gradient(110% 100% at 50% 115%, var(--tw-gradient-stops))',
       },
       animationDirection: {
-        reverse: "reverse",
+        reverse: 'reverse',
       },
       animation: {
-        blob: "blob 10s infinite",
-        "interchange-r": "interchange-r 20s infinite",
-        "interchange-l": "interchange-l 20s infinite",
+        blob: 'blob 10s infinite',
+        'interchange-r': 'interchange-r 20s infinite',
+        'interchange-l': 'interchange-l 20s infinite',
       },
       keyframes: {
         blob: {
-          "0%": {
-            transform: "translate(0, 0) scale(1)",
+          '0%': {
+            transform: 'translate(0, 0) scale(1)',
           },
-          "25%": {
-            transform: "translate(5vw, 5vh) scale(1.2)",
+          '25%': {
+            transform: 'translate(5vw, 5vh) scale(1.2)',
           },
-          "50%": {
-            transform: "translate(-10vw, 5vh) scale(1)",
+          '50%': {
+            transform: 'translate(-10vw, 5vh) scale(1)',
           },
-          "75%": {
-            transform: "translate(10vw, -20vh), scale(1.2)",
+          '75%': {
+            transform: 'translate(10vw, -20vh), scale(1.2)',
           },
-          "100%": {
-            transform: "translate(0, 0) scale(1)",
-          },
-        },
-        "interchange-l": {
-          "0%": {
-            transform: "translate(0, 0)",
-          },
-          "50%": {
-            transform: "translate(-60%, 0)",
-          },
-          "100%": {
-            transform: "translate(0, 0)",
+          '100%': {
+            transform: 'translate(0, 0) scale(1)',
           },
         },
-        "interchange-r": {
-          "0%": {
-            transform: "translate(0, 0)",
+        'interchange-l': {
+          '0%': {
+            transform: 'translate(0, 0)',
           },
-          "50%": {
-            transform: "translate(60%, 0)",
+          '50%': {
+            transform: 'translate(-60%, 0)',
           },
-          "100%": {
-            transform: "translate(0, 0)",
+          '100%': {
+            transform: 'translate(0, 0)',
+          },
+        },
+        'interchange-r': {
+          '0%': {
+            transform: 'translate(0, 0)',
+          },
+          '50%': {
+            transform: 'translate(60%, 0)',
+          },
+          '100%': {
+            transform: 'translate(0, 0)',
           },
         },
       },
@@ -87,8 +91,19 @@ module.exports = {
   },
   plugins: [
     function ({ addVariant }) {
-      addVariant("child", "& > *");
-      addVariant("child-hover", "& > *:hover");
+      addVariant('child', '& > *');
+      addVariant('child-hover', '& > *:hover');
+    },
+  ],
+  safelist: [
+    {
+      pattern:
+        /bg-(green|lime|sky|stone|emerald|cyan|yellow|slate|blue)-(50|300|700|900)/,
+      variants: ['dark'],
+    },
+    {
+      pattern: /bg-stone-(50|950)/,
+      variants: ['dark'],
     },
   ],
 };
